92b00467b2d5649a6206ec1159f54e18c7accba1
Joshua Byrd <phocks@gmail.com>
2023-04-21 16:43:01 +1000

    Local remote dev paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
-import relativeLinks from 'astro-relative-links';
+
+const { NODE_ENV } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
+const isProduction = NODE_ENV === "production";
 
 // Integrations
 import react from "@astrojs/react";
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [relativeLinks(), react(), svelte()],
-  base: "/res/sites/news-projects/astro-aunty-test/main/"
+  integrations: [react(), svelte()],
+  base: isProduction ? "/res/sites/news-projects/astro-aunty-test/main/" : "/"
 });
--- aunty.config.js
+++ aunty.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  // type: '<project_type>',
-  // aunty command configuration
-  build: { to: "dist" },
-  // serve: {…},
-  // deploy: [{…}],
-  // // internal tools configuration
-  // babel: {…},
-  // jest: {…},
-  // webpack: {…},
-  // webpackDevServer: {…}
+  build: { to: "dist" }
 };
