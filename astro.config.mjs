89581a9a9e0007a9941511e8d9758bbd854027cc
Joshua Byrd <phocks@gmail.com>
2023-04-24 13:52:45 +1000

    Get path from deploy config aunty

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 
+// Load local environment variables
 const { NODE_ENV } = loadEnv(import.meta.env.MODE, process.cwd(), "");
-
 const isProduction = NODE_ENV === "production";
+
+// Get deploy data and public base path
+import { targets } from ".aunty/deploy.json";
+const base = getBase(targets);
 
 // Integrations
 import react from "@astrojs/react";
@@ -12,5 +16,13 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), svelte()],
-  base: isProduction ? "/res/sites/news-projects/astro-aunty-test/main/" : "/"
+  base: isProduction ? base : "/"
 });
+
+// Functions
+
+function getBase(targets) {
+  const contentFtpTarget = targets.find(t => t.profile === "contentftp");
+  const { to } = contentFtpTarget;
+  return to.replace(/\/www/, "");
+}
